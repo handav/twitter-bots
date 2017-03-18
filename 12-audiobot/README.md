02085490d7f550cfddfc03949e0753c68ea1ad8d
handav <hannahdavis0220@gmail.com>
2017-03-17 22:40:46 -0400

    replacing lesson 12

--- 12-audiobot/README.md
+++ 12-audiobot/README.md
@@ -1,3 +1,7 @@
+Timidity must be installed on the system before this lesson.
+
+brew install timidity
+
 In the final bot lesson, we'll create an audio file composed based on a tweet that mentions us. Because Twitter only supports uploading audio in video form, we'll learn how to create a video from the MIDI file and post it to Twitter. 
 
 For more exciting things you can do with Natural Language Processing, see the course on Natural: https://egghead.io/courses/natural-language-processing-in-javascript-with-natural
@@ -6,7 +10,7 @@ A note: Image must not be larger than 1920x1080 and no more than 1,310,720 pixel
 
 Additional Resources:
 
-For Midi:
+Composing Midi:
 https://github.com/dingram/jsmidgen
 
 Video from Audio creation: 
--- 12-audiobot/index.js
+++ 12-audiobot/index.js
@@ -1,10 +1,12 @@
 var Twit = require('twit');
-var fs = require('fs');
 var rita = require('rita');
 var midi = require('jsmidgen');
-var ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+var fs = require('fs');
+var path = require('path');
+var child_process = require('child_process');
+var ffmpegPath = require('@ffmpeg-installer/ffmpeg');
 var ffmpeg = require('fluent-ffmpeg');
-ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfmpegPath(ffmpegPath.path);
 
 var bot = new Twit({
     consumer_key: process.env.LEARNINGBOT_CONSUMER_KEY,
@@ -14,11 +16,22 @@ var bot = new Twit({
     timeout_ms: 60 * 1000
 });
 
+var bot_username = '@bots_with_han';
+
+var imgFn = path.join(process.cwd(), 'black.jpg');
+var midiFn = path.join(process.cwd(), 'output.mid');
+var wavFn = path.join(process.cwd(), 'output.wav');
+var vidFn = path.join(process.cwd(), 'output.mp4');
+
 function hasNoStopwords(token){
     var stopwords = ['@', 'RT', 'http'];
     return stopwords.every(function(sw){
         return !token.includes(sw);
     });
+}
+
+function isNotPunctuation(token){
+    return !rita.RiTa.isPunctuation(token);
 }
 
 function cleanText(text){
@@ -32,20 +45,13 @@ function getPartsOfSpeech(text){
     return rita.RiTa.getPosTags(text);
 }
 
-function isNotPunctuation(tag){
-    return !rita.RiTa.isPunctuation();
-}
-
 function compose(taggedTweet, track){
-    var notes = taggedTweet.map(function(tag, i){
+    var notes = taggedTweet.map(function(tag){
         if (tag.includes('nn') || tag.includes('i')){
             return 'e4';
         }
         if (tag.includes('vb')){
             return 'g4';
-        }
-        if (i === taggedTweet.length -1){
-            return 'c3';
         }
         return 'c4';
     });
@@ -55,35 +61,78 @@ function compose(taggedTweet, track){
     return track;
 }
 
-function createMidi(tweet, midiFn){
+function createMidi(tweet, midiFn, cb){
     var file = new midi.File();
     var track = new midi.Track();
     file.addTrack(track);
-    var taggedTweet = getPartsOfSpeech(cleanText(tweet.text));
-    taggedTweet = taggedTweet.filter(isNotPunctuation);
+    var cleanedText = rita.RiTa
+        .tokenize(cleanText(tweet.text))
+        .filter(isNotPunctuation)
+        .join(' ');
+    var taggedTweet = getPartsOfSpeech(cleanedText);
     compose(taggedTweet, track);
-    fs.writeFileSync(midiFn, file.toBytes(), 'binary');
+    fs.writeFile(midiFn, file.toBytes(), {encoding: 'binary'}, cb);
 }
 
-function createVideo(imgFn, midiFn, vidFn, cb){
+function convertMidiToWav(midiFn, wavFn, cb){
+    var command = 'timidity --output-24bit -A120 '+midiFn + ' -Ow -o ' + wavFn;
+    child_process.exec(command, {}, function(err, stdout, stderr){
+        if (err){
+            cb(err);
+        }else{
+            cb(null);
+        }
+    });    
+}
+
+
+function createVideo(imgFn, wavFn, vidFn, cb){
     ffmpeg()
         .on('end', function(){
             cb(null);
         })
-        .on('error', function(err){
+        .on('error', function(err, stdout, stderr){
             cb(err);
         })
         .input(imgFn)
         .inputFPS(1/6)
-        .input(midiFn)
+        .input(wavFn)
         .output(vidFn)
         .outputFPS(30)
         .run();
 }
 
-function createMedia(tweet, imgFn, midiFn, vidFn, cb){
-    createMidi(tweet, midiFn);
-    createVideo(imgFn, midiFn, vidFn, cb);
+
+
+
+
+
+function createMedia(tweet, imgFn, midiFn, wavFn, vidFn, cb){
+    createMidi(tweet, midiFn, function(err, result){
+        if (err){
+            console.log(err);
+        }else{
+            convertMidiToWav(midiFn, wavFn, function(err){
+                if (err){
+                    console.log(err);
+                }else{
+                    console.log('Midi converted!');
+                    createVideo(imgFn, wavFn, vidFn, cb);
+                }
+            })
+        }
+    });
+}
+
+function deleteWav(wavFn, cb){
+    var command = 'rm '+ wavFn;
+    child_process.exec(command, {}, function(err, stdout, stderr){
+        if (err){
+            cb(err);
+        }else{
+            cb(null);
+        }
+    });
 }
 
 function postStatus(params){
@@ -91,22 +140,23 @@ function postStatus(params){
         if (err){
             console.log(err);
         }else{
-            console.log('Bot posted!');
+            console.log('Bot has posted!');
         }
     });
 }
 
+
 function uploadMedia(tweet, vidFn){
-    var filePath = __dirname + '/' + vidFn;
-    bot.postMediaChunked({file_path: filePath}, function(err, data, response){
+    bot.postMediaChunked({file_path: vidFn}, function(err, data, response){
         if (err){
             console.log(err);
         }else{
-            console.log(data);
-            tweet.text = tweet.text.split('@bots_with_han').join(' ').trim();
+            var stat = tweet.text.split(bot_username)
+                .join(' ')
+                .trim();
             var params = {
-                status: '@'+tweet.user.screen_name+' '+tweet.text,
-                in_reply_to_ID: tweet.in_reply_to_ID,
+                status: '@'+tweet.user.screen_name+' '+stat,
+                in_reply_to_status_id: tweet.id_str,
                 media_ids: data.media_id_string
             }
             postStatus(params);
@@ -114,11 +164,8 @@ function uploadMedia(tweet, vidFn){
     });
 }
 
-var imgFn = 'black.jpg';
-var midiFn = 'output.mid';
-var vidFn = 'output.mp4';
 
-var stream = bot.stream('statuses/filter', {track: '@bots_with_han'});
+var stream = bot.stream('statuses/filter', {track: bot_username});
 
 stream.on('connecting', function(response){
     console.log('connecting...');
@@ -134,16 +181,26 @@ stream.on('error', function(err){
 
 stream.on('tweet', function(tweet){
     if (tweet.text.length > 0){
-        createMedia(tweet, imgFn, midiFn, vidFn, function(err){
+        createMedia(tweet, imgFn, midiFn, wavFn, vidFn, function(err){
             if (err){
                 console.log(err);
             }else{
-                console.log('Video created!');
-                uploadMedia(tweet, vidFn);
+                console.log('Media created!');
+                deleteWav(wavFn, function(err){
+                    if (err){
+                        console.log(err);
+                    }else{
+                        uploadMedia(tweet, vidFn);
+                    }
+                });
             }
         });
     }
 });
+
+
+
+
 
 
 
